28c34bb5b479172aedd98e204136c5ee76b1aa86
Eloy Durán <eloy.de.enige@gmail.com>
2016-04-19 13:46:47 +0200

    [Pods] Add Artsy fonts dependency.
    But don’t actually check the private font files in.

---
+++ Example/Pods/Target Support Files/Artsy+UIFonts/Artsy+UIFonts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Artsy_UIFonts : NSObject
+@end
+@implementation PodsDummy_Artsy_UIFonts
+@end
---
+++ Example/Pods/Target Support Files/Artsy+UIFonts/Artsy+UIFonts-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UIFont+ArtsyFonts.h"
+
+FOUNDATION_EXPORT double Artsy_UIFontsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Artsy_UIFontsVersionString[];
+
