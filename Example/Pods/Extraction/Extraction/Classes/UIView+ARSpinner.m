2c622979ad56596e69e90c361bf6baf3c48263b0
Maxim Cramer <mennenia@gmail.com>
2016-06-02 12:46:09 +0100

    [Pods] Update Extraction

--- Example/Pods/Extraction/Extraction/Classes/UIView+ARSpinner.m
+++ Example/Pods/Extraction/Extraction/Classes/UIView+ARSpinner.m
@@ -14,10 +14,10 @@ CGFloat RotationDuration = 0.9;
 - (void)ar_startSpinning:(NSInteger)times
 {
     CATransform3D rotationTransform = CATransform3DMakeRotation(-1.01f * M_PI, 0, 0, 1.0);
-
+    
     CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform"];
     objc_setAssociatedObject(self, @selector(ar_startSpinning:), rotationAnimation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-
+    
     rotationAnimation.toValue = [NSValue valueWithCATransform3D:rotationTransform];
     rotationAnimation.duration = RotationDuration;
     rotationAnimation.cumulative = YES;
