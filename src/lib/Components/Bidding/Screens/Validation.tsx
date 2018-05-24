9232ddd43c9e54a2da037ffe7ff3cf70bd44a191
eriks <erik@artsymail.com>
2018-05-24 16:16:52 -0400

    update naming

--- src/lib/Components/Bidding/Screens/Validation.tsx
+++ src/lib/Components/Bidding/Screens/Validation.tsx
@@ -1,3 +1,4 @@
+// currently this assumes every input is a string
 interface InputStrings {
   [everyProp: string]: string
 }
@@ -5,9 +6,10 @@ interface InputStrings {
 type ValidationRule = (val: string) => boolean
 type Errors<Inputs> = Partial<{ [P in keyof Inputs]: string }>
 type Validator = (val: string) => string | void
-type ValidatorWithDefault = (msg?: string) => Validator
+type ValidatorWithMessage = (msg?: string) => Validator
+// type ValidatorWithArg = (arg: any, msg?: string) => Validator
 
-type ValidationSchema<Inputs> = Partial<{ [P in keyof Inputs]: Validator[] }>
+export type ValidationSchema<Inputs> = Partial<{ [P in keyof Inputs]: Validator[] }>
 
 export const composeValidator: (rule: ValidationRule, msg: string) => Validator = (rule, msg) => val => {
   if (!rule(val)) {
@@ -18,10 +20,17 @@ export const composeValidator: (rule: ValidationRule, msg: string) => Validator 
 // Rules
 const validateEmail: ValidationRule = val => val.indexOf("@") !== -1
 const validateRequired: ValidationRule = val => val.length > 0
+const validateNumber: ValidationRule = val => Number.isNaN(Number.parseFloat(val))
+const validateInt: ValidationRule = val => Number.isNaN(Number.parseInt(val))
+// const validateMinLength: (min: number) => ValidationRule = min => val => val.length >= min
 
 // Validators from Functions
-export const isRequired: ValidatorWithDefault = (message = "Required") => composeValidator(validateRequired, message)
-export const isEmail: ValidatorWithDefault = (msg = "Valid Email Required") => composeValidator(validateEmail, msg)
+export const isRequired: ValidatorWithMessage = (message = "Required") => composeValidator(validateRequired, message)
+export const isEmail: ValidatorWithMessage = (msg = "Valid Email Required") => composeValidator(validateEmail, msg)
+export const isNumber: ValidatorWithMessage = (msg = "Number Required") => composeValidator(validateNumber, msg)
+export const isInt: ValidatorWithMessage = (msg = "Integer Required") => composeValidator(validateInt, msg)
+// export const minLength: (min: number, msg?: string) => ValidatorWithArg = (min, msg) =>
+// composeValidator(validateMinLength(min), msg || `Min Length ${min} required`)
 
 const rules = {
   name: [isRequired()],
@@ -32,7 +41,7 @@ const rules = {
 // export const validateField = (name, values, schema) => {
 //   const validators = schema[name]
 // }
-export class Foo<I extends InputStrings> {
+export class Validation<I extends InputStrings> {
   public schema: ValidationSchema<I>
   constructor(schema: ValidationSchema<I>) {
     this.schema = schema
