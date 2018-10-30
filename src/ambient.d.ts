7aa00ad2393ad811a113d5f5f495336c35dc86b7
Orta Therox <orta.therox@gmail.com>
2018-10-30 17:02:15 -0400

    Update the React Native version, and get types up to date

---
+++ src/ambient.d.ts
@@ -0,0 +1,19 @@
+// Exists in JS runtime, needed by relay
+interface File {
+  _?: boolean
+}
+
+// Exists in JS runtime *when running in a web inspector
+interface Console {
+  groupCollapsed(message?: any, ...optionalParams: any[]): void
+  groupEnd(): void
+}
+
+// Needed by styled components, normally comes from the DOM d.ts
+interface StyleSheet {
+  _?: boolean
+}
+
+interface Node {
+  _?: boolean
+}
