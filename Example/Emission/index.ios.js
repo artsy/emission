2fc9a72d2d3ad4e83adcae9f9c72e751a5b58e36
Eloy Durán <eloy.de.enige@gmail.com>
2019-03-15 12:49:19 +0100

    enable RN bridge message logging

--- Example/Emission/index.ios.js
+++ Example/Emission/index.ios.js
@@ -1,3 +1,13 @@
+import MessageQueue from "react-native/Libraries/BatchedBridge/MessageQueue.js"
+
+const spyFunction = msg => {
+  if (!(msg.module === "RCTDeviceEventEmitter" && msg.args[0] === "websocketMessage")) {
+    console.log(msg)
+  }
+}
+
+MessageQueue.spy(spyFunction)
+
 import "./index.storybooks"
 import "../../src/lib/Components/Storybooks"
 
