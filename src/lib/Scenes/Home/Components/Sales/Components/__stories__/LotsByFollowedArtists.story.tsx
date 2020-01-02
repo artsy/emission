da0fc7d2743e63a31a65ebdee1e79172393e247d
brainbicycle <brian.beckerle@artsymail.com>
2020-01-02 17:19:39 -0500

    Add storybook component for auction zero state

--- src/lib/Scenes/Home/Components/Sales/Components/__stories__/LotsByFollowedArtists.story.tsx
+++ src/lib/Scenes/Home/Components/Sales/Components/__stories__/LotsByFollowedArtists.story.tsx
@@ -3,7 +3,8 @@ import renderWithLoadProgress from "lib/utils/renderWithLoadProgress"
 import React from "react"
 import { SalesRenderer } from "../../Relay/SalesRenderer"
 import LotsByFollowedArtists from "../LotsByFollowedArtists"
+import { ZeroState } from "../ZeroState"
 
-storiesOf("Home/Relay/Auctions").add("LotsByFollowedArtists", () => (
-  <SalesRenderer render={renderWithLoadProgress(LotsByFollowedArtists)} />
-))
+storiesOf("Home/Relay/Auctions")
+  .add("LotsByFollowedArtists", () => <SalesRenderer render={renderWithLoadProgress(LotsByFollowedArtists)} />)
+  .add("Zero State for Auctions", () => <ZeroState />)
