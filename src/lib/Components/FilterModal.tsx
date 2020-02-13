4c4f4e7d98af6f4109234abf813fd8d59eb0b5ae
Ashley Jelks <arjelks@gmail.com>
2020-02-13 15:19:52 +0100

    adds sort options to modal

--- src/lib/Components/FilterModal.tsx
+++ src/lib/Components/FilterModal.tsx
@@ -1,6 +1,14 @@
-import { Box, Button, CloseIcon, Flex, Sans } from "@artsy/palette"
+import { ArrowRightIcon, Box, Button, CloseIcon, color, Flex, Sans, Serif } from "@artsy/palette"
+import {
+  MediumOptions,
+  PriceRangeOptions,
+  SizeOptions,
+  SortOptions,
+  TimePeriodOptions,
+  WaysToBuyOptions,
+} from "lib/data/ArtworkFilterOptions"
 import React from "react"
-import { LayoutAnimation, Modal as RNModal, TouchableWithoutFeedback, ViewProperties } from "react-native"
+import { FlatList, LayoutAnimation, Modal as RNModal, TouchableWithoutFeedback, ViewProperties } from "react-native"
 import styled from "styled-components/native"
 
 interface ModalProps extends ViewProperties {
@@ -11,29 +19,29 @@ interface ModalProps extends ViewProperties {
 // TODO:  Define a TypeScript interface to represent possible filter states (take a look at the Pick TypeScript generic)
 interface State {
   isComponentMounted: boolean
+  sortableItems: Array<{ type: string; data: any }>
+  currentSortOrder: SortOptions
+  waysToBuySortOrder: WaysToBuyOptions
+  mediumSortOrder: MediumOptions
+  priceRangeSortOrder: PriceRangeOptions
+  sizeSortOrder: SizeOptions
+  timePeriodSortOrder: TimePeriodOptions
 }
-
-const ModalBackgroundView = styled.View`
-  background-color: #00000099;
-  flex: 1;
-  flex-direction: column;
-`
-
-const ModalInnerView = styled.View<{ visible: boolean }>`
-  flex-direction: column;
-  width: 100%;
-  background-color: white;
-  height: ${({ visible }) => (visible ? "auto" : "0")};
-  padding: ${({ visible }) => (visible ? "20px" : "0")};
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-`
 
 export class FilterModal extends React.Component<ModalProps, State> {
   constructor(props) {
     super(props)
 
-    this.state = { isComponentMounted: false }
+    this.state = {
+      isComponentMounted: false,
+      sortableItems: [],
+      currentSortOrder: SortOptions.Default,
+      waysToBuySortOrder: WaysToBuyOptions.All,
+      mediumSortOrder: MediumOptions.All,
+      priceRangeSortOrder: PriceRangeOptions.All,
+      sizeSortOrder: SizeOptions.All,
+      timePeriodSortOrder: TimePeriodOptions.All,
+    }
   }
 
   componentDidMount() {
@@ -41,6 +49,49 @@ export class FilterModal extends React.Component<ModalProps, State> {
       LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
       this.setState({ isComponentMounted: true })
     }, 100)
+
+    const sortableItems = []
+
+    sortableItems.push({
+      type: "Sort by",
+      data: {
+        sort: [],
+      },
+    })
+    sortableItems.push({
+      type: "Ways to buy",
+      data: {
+        sort: [],
+      },
+    })
+    sortableItems.push({
+      type: "Medium",
+      data: {
+        sort: [],
+      },
+    })
+    sortableItems.push({
+      type: "Price range",
+      data: {
+        sort: [],
+      },
+    })
+    sortableItems.push({
+      type: "Size",
+      data: {
+        sort: [],
+      },
+    })
+    sortableItems.push({
+      type: "Time period",
+      data: {
+        sort: [],
+      },
+    })
+
+    this.setState({
+      sortableItems,
+    })
   }
 
   closeModal() {
@@ -49,7 +100,46 @@ export class FilterModal extends React.Component<ModalProps, State> {
     }
   }
 
+  getDefaultSort(type) {
+    switch (type) {
+      case "Sort by":
+        return this.state.currentSortOrder
+      case "Ways to buy":
+        return this.state.waysToBuySortOrder
+      case "Medium":
+        return this.state.mediumSortOrder
+      case "Price range":
+        return this.state.priceRangeSortOrder
+      case "Size":
+        return this.state.sizeSortOrder
+      case "Time period":
+        return this.state.timePeriodSortOrder
+      default:
+        return null
+    }
+  }
+  getSortItem(type) {
+    return (
+      <SortRowItem>
+        <Flex p={2} flexDirection="row" justifyContent="space-between" flexGrow={1}>
+          <Serif size="3">{type}</Serif>
+          <Flex flexDirection="row">
+            <Serif color={color("black60")} size="3">
+              {this.getDefaultSort(type)}
+            </Serif>
+            <ArrowRightIcon fill="black30" ml={0.3} mt={0.3} />
+          </Flex>
+        </Flex>
+      </SortRowItem>
+    )
+  }
+  renderSortItem = ({ item: { type } }) => {
+    return this.getSortItem(type)
+  }
+
   render() {
+    const { sortableItems } = this.state
+
     return (
       <RNModal animationType="fade" transparent={true} visible={this.props.visible}>
         <TouchableWithoutFeedback>
@@ -60,26 +150,37 @@ export class FilterModal extends React.Component<ModalProps, State> {
                 <ModalInnerView visible={this.state.isComponentMounted}>
                   <Flex flexDirection="row" justifyContent="space-between">
                     <Flex alignItems="flex-end" mt={0.5} mb={2}>
-                      <Box onTouchStart={() => this.closeModal()}>
+                      <Box ml={2} mt={2} onTouchStart={() => this.closeModal()}>
                         <CloseIcon fill="black100" />
                       </Box>
                     </Flex>
-                    <Sans weight="medium" size="4">
+                    <Sans mt={2} weight="medium" size="4">
                       Filter
                     </Sans>
-                    <Sans size="4">Clear all</Sans>
+                    <Sans mr={2} mt={2} size="4">
+                      Clear all
+                    </Sans>
                   </Flex>
                   {this.props.children}
-                  <Button
-                    onPress={() => {
-                      this.closeModal()
-                    }}
-                    block
-                    width={100}
-                    variant="secondaryOutline"
-                  >
-                    Ok
-                  </Button>
+                  <Flex flexDirection="row">
+                    <FlatList
+                      keyExtractor={(_item, index) => String(index)}
+                      data={sortableItems}
+                      renderItem={item => <Box>{this.renderSortItem(item)}</Box>}
+                    />
+                  </Flex>
+                  <Box p={2}>
+                    <Button
+                      onPress={() => {
+                        this.closeModal()
+                      }}
+                      block
+                      width={100}
+                      variant="secondaryOutline"
+                    >
+                      Ok
+                    </Button>
+                  </Box>
                 </ModalInnerView>
               </>
             </TouchableWithoutFeedback>
@@ -89,3 +190,27 @@ export class FilterModal extends React.Component<ModalProps, State> {
     )
   }
 }
+
+const ModalBackgroundView = styled.View`
+  background-color: #00000099;
+  flex: 1;
+  flex-direction: column;
+`
+
+const ModalInnerView = styled.View<{ visible: boolean }>`
+  flex-direction: column;
+  background-color: white;
+  height: ${({ visible }) => (visible ? "auto" : "0")};
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`
+const SortRowItem = styled(Flex)`
+  flex-direction: row;
+  justify-content: space-between;
+  border: solid 1px ${color("black10")};
+  border-bottom-width: 0;
+  border-right-width: 0;
+  border-left-width: 0;
+  flex: 1;
+  width: 100%;
+`
